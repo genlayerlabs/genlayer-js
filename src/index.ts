5dc0d81bfa89111f785c07e07cfa2f2880946ed7
Edinaldo Junior <epsjunior@icloud.com>
2025-07-24 13:56:54 -0300

    chore: export order

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 // src/index.ts
 export {createClient} from "./client/client";
 export {createAccount, generatePrivateKey} from "./accounts/account";
-export * as chains from "./chains";
-export * as abi from "./abi";
 export {
   decodeInputData,
   decodeTransaction,
   simplifyTransactionReceipt,
   decodeLocalnetTransaction
 } from "./transactions/decoders";
+export * as chains from "./chains";
+export * as abi from "./abi";
