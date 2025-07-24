2bb03f224fa879b1d3b987f60f7cf1f801cbbadb
Edinaldo Junior <epsjunior@icloud.com>
2025-07-24 13:13:54 -0300

    feat: new decoders accessible by genlayer-js users

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ export {createClient} from "./client/client";
 export {createAccount, generatePrivateKey} from "./accounts/account";
 export * as chains from "./chains";
 export * as abi from "./abi";
+export {
+  decodeInputData,
+  decodeTransaction,
+  simplifyTransactionReceipt,
+  decodeLocalnetTransaction
+} from "./transactions/decoders";
